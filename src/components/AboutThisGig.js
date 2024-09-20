38655154e35c5a4bb8636cafbcf14e40656be64e
Ryan Locke <rclocke1@myseneca.ca>
2024-09-20 15:02:57 -0400

    added AboutThisGig component

---
+++ src/components/AboutThisGig.js
@@ -0,0 +1,180 @@
+import React from "react";
+import { Card, ListGroup, Row, Col } from "react-bootstrap";
+
+export default function AboutThisGig(){
+
+    return(
+        <Card>
+            <Card.Body>
+                <Card.Title>About This Gig</Card.Title>
+                <Card.Text>
+                    Hi there! My name is Windlist and I draw cartoons and anime of fanart, oc and character concepts. I specialize 
+                    in cartoon and anime styles but I don't mind expanding my horizons a bit. All I ask is if you can message me in 
+                    advance if this is the case :)
+                </Card.Text>
+                <Card.Text>
+                        What I do:<br/>female/male/they/them (LGBTQ+ friendly, everyone is welcomed) fanart
+                </Card.Text>
+                <Card.Text>  
+                        Please note: ALL of my prices are negotiable, as I understand the kind of times we are living in.
+                </Card.Text>
+                <Card.Text as='h6'>
+                        Additional offers available
+                </Card.Text>
+                <ListGroup>
+                    <ListGroup.Item className="border-0">
+                        <ul >
+                            <li className="mb-2">Extra poses ($20 per pose) **NOTE: Premium purchases include 1 free additional pose**</li>
+                            <li className="mb-2">FREE Revisions</li>
+                            <li className="mb-2">Animated GIFS (discord sticker sizes available) **Extra $20**</li>
+                            <li className="mb-2">FREE HD artwork</li>
+                            <li>Prices include regular shipping, extra shipping cost varies.</li>
+                        </ul>
+                    </ListGroup.Item>
+                </ListGroup>
+                <Card.Text>
+                    **PLEASE be as detailed as possible as I want to meet your expectations as best as I can. I have provided 
+                    questions for this reason whilst you submit your request so please answer them as detailed as possible**
+                </Card.Text>
+                <Card.Text>
+                    I look forward to working with you!
+                </Card.Text>
+                <Card.Text>
+                    Sincerely, <br />Windlist
+                </Card.Text>
+                <hr />
+                <Row className="g-0">
+                    <Col>
+                        <p>Main type <br /> Cartoon</p>
+                    </Col>
+                    <Col>
+                        <p>Illustration style <br /> Other</p>
+                    </Col>
+                    <Col>
+                        <p>Image file format <br /> JPG</p>
+                    </Col>
+                </Row>
+            </Card.Body>
+        </Card>
+    )
+}
+
+
+
+// import React from "react";
+// import { Card, ListGroup, Row, Col } from "react-bootstrap";
+
+// export default function AboutThisGig(){
+
+//     return(
+//         <Card>
+//             <Card.Body>
+//                 <Card.Title>About This Gig</Card.Title>
+//                 <Card.Text>
+//                     Hi there! My name is Windlist and I draw cartoons and anime of fanart, oc and character concepts. I specialize in 
+//                     cartoon and anime styles but I don't mind expanding my horizons a bit. All I ask is if you can message me in advance
+//                     if this is the case :) <br /> <br />
+//                     What I do:<br/>
+//                     female/male/they/them (LGBTQ+ friendly, everyone is welcomed) fanart <br /> <br />
+//                     Please note: ALL of my prices are negotiable, as I understand the kind of times we are living in. <br/>
+//                 </Card.Text> 
+//                 <h6>Additional offers available</h6>
+//                 <ListGroup>
+//                     <ListGroup.Item className="border-0">
+//                         <ul >
+//                             <li className="mb-2">Extra poses ($20 per pose) **NOTE: Premium purchases include 1 free additional pose**</li>
+//                             <li className="mb-2">FREE Revisions</li>
+//                             <li className="mb-2">Animated GIFS (discord sticker sizes available) **Extra $20**</li>
+//                             <li className="mb-2">FREE HD artwork</li>
+//                             <li>Prices include regular shipping, extra shipping cost varies.</li>
+//                         </ul>
+//                     </ListGroup.Item>
+//                 </ListGroup>
+//                 <Card.Text>
+//                     **PLEASE be as detailed as possible as I want to meet your expectations as best as I can. I have provided questions 
+//                     for this reason whilst you submit your request so please answer them as detailed as possible** <br /> <br />
+//                     I look forward to working with you! <br /> <br />
+//                     Sincerely, <br />
+//                     Windlist
+//                 </Card.Text>
+//                 <hr />
+//                 <Row className="g-0">
+//                     <Col>
+//                         <p>Main type <br /> Cartoon</p>
+//                     </Col>
+//                     <Col>
+//                         <p>Illustration style <br /> Other</p>
+//                     </Col>
+//                     <Col>
+//                         <p>Image file format <br /> JPG</p>
+//                     </Col>
+//                 </Row>
+//             </Card.Body>
+//         </Card>
+//     )
+// }
+
+
+
+
+
+
+
+
+// import React from "react";
+// import { Card, ListGroup, Row, Col } from "react-bootstrap";
+
+// export default function AboutThisGig(){
+
+//     return(
+//         <Card>
+//             <Card.Body>
+//                 <Card.Title>About This Gig</Card.Title>
+//                 <Card.Text>
+//                     <p>Hi there! My name is Windlist and I draw cartoons and anime of fanart, oc and character concepts. I specialize 
+//                         in cartoon and anime styles but I don't mind expanding my horizons a bit. All I ask is if you can message me in 
+//                         advance if this is the case :)</p>
+//                     <p>What I do:<br/>female/male/they/them (LGBTQ+ friendly, everyone is welcomed) fanart</p>
+//                     <p>Please note: ALL of my prices are negotiable, as I understand the kind of times we are living in.</p>
+//                     <p1>Additional offers available</p1>
+//                     {/* <ListGroup>
+//                         <ListGroup.Item className="border-0">
+//                             Extra poses ($20 per pose) **NOTE: Premium purchases include 1 free additional pose**
+//                         </ListGroup.Item>
+//                         <ListGroup.Item className="border-0">FREE Revisions</ListGroup.Item>
+//                         <ListGroup.Item className="border-0">Animated GIFS (discord sticker sizes available) **Extra $20**</ListGroup.Item>
+//                         <ListGroup.Item className="border-0">FREE HD artwork</ListGroup.Item>
+//                         <ListGroup.Item className="border-0">Prices include regular shipping, extra shipping cost varies.</ListGroup.Item>
+//                     </ListGroup> */}
+//                     <ListGroup>
+//                         <ListGroup.Item className="border-0">
+//                             <ul >
+//                                 <li className="mb-2">Extra poses ($20 per pose) **NOTE: Premium purchases include 1 free additional pose**</li>
+//                                 <li className="mb-2">FREE Revisions</li>
+//                                 <li className="mb-2">Animated GIFS (discord sticker sizes available) **Extra $20**</li>
+//                                 <li className="mb-2">FREE HD artwork</li>
+//                                 <li>Prices include regular shipping, extra shipping cost varies.</li>
+//                             </ul>
+//                         </ListGroup.Item>
+//                     </ListGroup>
+//                     <p>**PLEASE be as detailed as possible as I want to meet your expectations as best as I can. I have provided 
+//                         questions for this reason whilst you submit your request so please answer them as detailed as possible**</p>
+//                     <p>I look forward to working with you!</p>
+//                     <p>Sincerely, <br />Windlist</p>
+//                 </Card.Text> 
+//                 <hr />
+//                 <Row className="g-0">
+//                     <Col>
+//                         <p>Main type <br /> Cartoon</p>
+//                     </Col>
+//                     <Col>
+//                         <p>Illustration style <br /> Other</p>
+//                     </Col>
+//                     <Col>
+//                         <p>Image file format <br /> JPG</p>
+//                     </Col>
+//                 </Row>
+//             </Card.Body>
+//         </Card>
+//     )
+// }
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,20 +1,25 @@
 import React from 'react'
-import { Container, Card } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
+import AboutThisGig from '../components/AboutThisGig'
 
 export default function About(){
     return(
-        <Container className='text-center mt-5'>
-            <Card>
-                <Card.Title>About Me</Card.Title>
-                <Card.Body>
-                    <div className='p-5 mb-4 bg-light rounded-3'>
-                        <h1>About Me</h1>
-                        <h3>Hi There! My name is Windlist and I create character art in anime and cartoon styles. I look forward to
-                        getting to work with you!</h3>
-                        <h3>Skills</h3>
-                    </div>
-                </Card.Body>
-            </Card>
+        // <Container className='text-center mt-5'>
+        //     {/* <Card>
+        //         <Card.Title>About Me</Card.Title>
+        //         <Card.Body>
+        //             <div className='p-5 mb-4 bg-light rounded-3'>
+        //                 <h1>About Me</h1>
+        //                 <h3>Hi There! My name is Windlist and I create character art in anime and cartoon styles. I look forward to
+        //                 getting to work with you!</h3>
+        //             </div>
+        //         </Card.Body>
+        //     </Card> */}
+        //     <AboutThisGig />
+        // </Container>
+
+        <Container className='mt-5'>
+            <AboutThisGig />
         </Container>
     )
 }
