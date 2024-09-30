76be7d80af0ab5672eb59643f6998b427dcdf4f9
Ryan Locke <rclocke1@myseneca.ca>
2024-09-30 16:41:49 -0400

    added contact form to Contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { Container, Button, Form } from 'react-bootstrap';
 
 export default function Contact() {
-    return (
-      
+  return (
         // <Container className="text-center mt-5">
         //   <div className="p-5 mb-4 bg-light rounded-3">
         //     <h1>Message Windlist</h1>
@@ -19,41 +18,40 @@ export default function Contact() {
         //   </div>
         // </Container>
 
-      <Container className="mt-5">
-        <h2 className="text-center mb-4">Message Windlist</h2>
-        <Form>
-          
-          {/* <Form.Group className="mb-3" controlId="formName">
-            <Form.Label>Your Name</Form.Label>
-            <Form.Control type="text" placeholder="Enter your name" required />
-          </Form.Group>
+    <Container className="mt-5">
+      <h2 className="text-center mb-4">Message Windlist</h2>
+      <Form>
+        
+        <Form.Group className="mb-3" controlId="formName">
+          <Form.Label>Your Name</Form.Label>
+          <Form.Control type="text" placeholder="Enter your name" required />
+        </Form.Group>
 
 
-          <Form.Group className="mb-3" controlId="formEmail">
-            <Form.Label>Email Address</Form.Label>
-            <Form.Control type="email" placeholder="Enter your email" required />
-          </Form.Group>
+        <Form.Group className="mb-3" controlId="formEmail">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control type="email" placeholder="Enter your email" required />
+        </Form.Group>
 
-          
-          <Form.Group className="mb-3" controlId="formSubject">
-            <Form.Label>Subject</Form.Label>
-            <Form.Control type="text" placeholder="Subject" required />
-          </Form.Group> */}
+        
+        <Form.Group className="mb-3" controlId="formSubject">
+          <Form.Label>Subject</Form.Label>
+          <Form.Control type="text" placeholder="Subject" required />
+        </Form.Group>
 
 
-          <Form.Group className="mb-3" controlId="formMessage">
-            <Form.Label>Message</Form.Label>
-            <Form.Control as="textarea" rows={4} placeholder="Write your message here" required />
-          </Form.Group>
+        <Form.Group className="mb-3" controlId="formMessage">
+          <Form.Label>Message</Form.Label>
+          <Form.Control as="textarea" rows={4} placeholder="Write your message here" required />
+        </Form.Group>
 
-          
-          <Button variant="primary" type="submit">
-            Send Message
-          </Button>
-        </Form>
+        
+        <Button variant="primary" type="submit">
+          Send Message
+        </Button>
+      </Form>
     </Container>
-      );
-    
+  );
 };
 
 
