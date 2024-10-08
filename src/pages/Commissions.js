94fc35cbc7b560c57b1f453eaf856684254ce823
Ryan Locke <rclocke1@myseneca.ca>
2024-10-07 21:54:53 -0400

    lifted state

--- src/pages/Commissions.js
+++ src/pages/Commissions.js
@@ -1,10 +1,31 @@
 import React from "react";
+import { useState } from "react";
 import PackageOptions from "../components/PackageOptions";
+import OrderOptions from "../components/OrderOptions";
+import { packageDetails } from "../data/packageDetails";
 
 export default function Commissions(){
+
+    // const [selectedPackage, setSelectedPackage] = useState(null);
+    const [selectedPackage, setSelectedPackage] = useState(packageDetails['basic']);
+
+    const [show, setShow] = useState(false);
+    
+    // Function to open the offcanvas
+    //const handleShow = () => setShow(true);
+
+    function handleShow(packageType){
+        setSelectedPackage(packageDetails[packageType])
+        setShow(true)
+    };
+
+    // Function to close the offcanvas
+    const handleClose = () => setShow(false);
+
     return(
         <>
-        <PackageOptions />
+        <PackageOptions handleShow={handleShow}/>
+        <OrderOptions show={show} handleClose={handleClose} selectedPackage={selectedPackage}/>
         </>
         
     )
